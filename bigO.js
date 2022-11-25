af2c8e6eae6e8ade976e1129e67cb48f101d1f56
Monte Honarchian <montehonarchian@gmail.com>
2022-11-25 05:09:44 -0800

    BigO continuation 2

--- bigO.js
+++ bigO.js
@@ -92,14 +92,99 @@
 // logAllPairsOfArray(boxes)
 // //When nested  we get O(n * n) --> O(n^2) - Quadratic Time
 
-function allSums(numbers) {
+// function allSums(numbers) {
 
-  console.log('these are the numbers:');
-  numbers.forEach(function(number) {
-    console.log(number);
-  });
+//   console.log('these are the numbers:');
+//   numbers.forEach(function(number) {
+//     console.log(number);
+//   });
 
-  console.log('these are the sums:');
+//   console.log('these are the sums:');
+//   numbers.forEach(function(firstNumber) {
+//     numbers.forEach(function(secondNumber) {
+//       console.log(firstNumber + secondNumber);
+//     });
+//   });
+// }
+
+// allSums( [1,2,3,4,5])
+
+// Big O = O(n + n^2) --> O(n^2)  ---> we drop the "n +" because  it is not dominant per rule #4 of Drop the Non-Dominants
+// in Big O we are focused on scalability, so the Dominant term is the term that has the most impact when the scale increases. So get
+// rid of the non dominants
+
+//BigO Cheatcheat
+// Big Os
+// O(1) Constant – no loops
+// O(log N) Logarithmic – usually searching algorithms have log n if they are sorted (Binary Search)
+// O(n) Linear – for loops, while loops through n items
+// O(n log(n)) Log Linear – usually sorting operations
+// O(n^2) Quadratic – every element in a collection needs to be compared to ever other element. Two
+// nested loops
+// O(2^n) Exponential – recursive algorithms that solves a problem of size N
+// O(n!) Factorial – you are adding a loop for every element
+
+// Iterating through half a collection is still O(n)
+// Two separate collections: O(a * b)
+
+// What Can Cause Time in a Function?
+// Operations (+, -, *, /)
+// Comparisons (<, >, ==)
+// Looping (for, while)
+// Outside Function call (function())
+
+// Rule Book
+// Rule 1: Always worst Case
+// Rule 2: Remove Constants
+// Rule 3:
+// • Different inputs should have different variables: O(a + b)
+// • A and B arrays nested would be: O(a * b)
+// + for steps in order
+// * for nested steps
+// Rule 4: Drop Non-dominant terms
+
+// What Causes Space Complexity?
+// • Variables
+// • Data Structures
+// • Function Call
+// • Allocations
+
+
+// Factorial Time O(n!)
+// Adding a nested loop for every element - Super Expensive to run, rarely will see it. If you see it something is wrong
+
+
+// What is good code?
+// 1. Readable
+// 2. Scalable - Speed(CPU) & Memory(RAM)
+
+
+// Which code is best?
+// Readable - readable, clean code that others can read that is maintainable.
+// Speed - we call time complexity has a big O time complexity that is efficient, scales well
+// Memory - use Big O again, but this time for space complexity rather than time complexity
+
+// Sacrifice Speed for Memory and vice versa
+
+//Programs execute in two ways to remember things: Heap and Stack
+// Heap is where we store variables taht we assign values to.
+// Stack is where we keep track of our function calls.
+
+// // Example: Space Complexity 
+// function boooo(n) {
+//   for (let i = 0; i< n.length; i++){
+//     console.log('booooo!');
+//   }
+// }
+
+// boooo([1,2,3,4,5]) // O(1) --> because the function has nothing adding space complexity, it's "raw" in a sense
+
+function arrayOfHiNTimes(n){
+  let hiArray = [];
+  for (let i = 0; i < n; i++){
+    hiArray[i] = 'hi';
+  }
+  return hiArray;
 }
 
-allSums( [1,2,3,4,5,6,7])
+arrayOfHiNTimes(6)
